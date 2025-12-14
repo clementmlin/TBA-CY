50b0fde7f393b5f72469f7b6cb9943801a5bca66
Yassine-Moussa <yassine.moussa@edu.esiee.fr>
2025-12-14 12:54:26 +0000

    régler des problèmes

--- game.py
+++ game.py
@@ -14,6 +14,8 @@ class Game:
         self.commands = {}
         self.player = None
         self.current_room = None
+    def play(self):
+        print(self.current_room.name)
     
 
     def setup(self):
